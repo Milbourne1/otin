0329e4162b991d524f9925d8da402b25612aa954
Milbourne1 <jona.mamaril.up@phinmaed.com>
2023-10-25 11:54:24 +0800

    gg commit

--- app/src/main/java/com/example/navigatioandbottom/LoginActivity.kt
+++ app/src/main/java/com/example/navigatioandbottom/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.navigatioandbottom
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import com.example.navigatioandbottom.databinding.ActivityLoginBinding
 
@@ -10,17 +11,33 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var databaseHelper: DatabaseHelper
-
+    private lateinit var database: LogIn_SignUpDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         databaseHelper = DatabaseHelper(this)
+        database = LogIn_SignUpDatabase(this)
 
         binding.loginButton.setOnClickListener {
             val loginUsername = binding.loginUsername.text.toString()
             val loginPassword = binding.loginPassword.text.toString()
+            val checkuser = database.checkuserpass(loginUsername, loginPassword)
+
+
+            if (TextUtils.isEmpty(loginUsername) || TextUtils.isEmpty(loginPassword)) {
+                Toast.makeText(this, "Add Username & Password", Toast.LENGTH_SHORT).show()
+            } else {
+                if (checkuser == true) {
+                    Toast.makeText(this, "Log In Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, homeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Wrong Password & Username", Toast.LENGTH_SHORT).show()
+                }
+            }
             loginDatabase(loginUsername, loginPassword)
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/navigatioandbottom/SignupActivity.kt
+++ app/src/main/java/com/example/navigatioandbottom/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.example.navigatioandbottom
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import com.example.navigatioandbottom.databinding.ActivitySignupBinding
 
@@ -10,7 +11,7 @@ class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
     private lateinit var databaseHelper: DatabaseHelper
-
+    private lateinit var data: LogIn_SignUpDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
@@ -18,26 +19,29 @@ class SignupActivity : AppCompatActivity() {
 
         databaseHelper = DatabaseHelper(this)
         binding.signupButton.setOnClickListener {
+
             val signupUsername = binding.signupUsername.text.toString()
             val signupPassword = binding.signupPassword.text.toString()
-            signupDatabase(signupUsername, signupPassword)
+            val savedata = data.insertdata(signupUsername, signupPassword)
+
+            if (TextUtils.isEmpty(signupPassword) || TextUtils.isEmpty(signupUsername)){
+                Toast.makeText(this, "Add Username or Password", Toast.LENGTH_SHORT).show()
+            }else{
+                if (savedata == true) {
+                    Toast.makeText(this, "Sign Up Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+
+
         }
         binding.loginRedirect.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
-    }
-    private fun signupDatabase(username:String, password: String){
-        val insertRowId = databaseHelper.insertUser(username, password)
-        if(insertRowId != -1L){
-            Toast.makeText(this, "Sign up Successful", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-
-        }else
-            Toast.makeText(this, "Sign up Failed", Toast.LENGTH_SHORT).show()
     }
 
 }
